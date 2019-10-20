41b8637904bb50bbc4167fbd4b28a42283e0692d
Gaiasys-Trilok <trilok@gaiasys.in>
2019-10-21 01:01:27 +0530

    express route parameters

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const app = express();
 app.get('/', (req,res)=> {
     res.send('yes i m available')
 });
-app.listen(3000, ()=> {
-    console.log('listenning on 3000')
+app.get('/api', (req, res) => {
+    res.send(['Agra', 'Delhi', 'Kanpur']);
+});
+app.get('/api/:id', (req, res) => {
+    res.send(req.params.id);
+});
+app.get('/api/:title/:publisher', (req, res) => {
+    res.send(req.params);
+});
+app.get('/api/:title/:publisher', (req, res) => {
+    res.send([req.params, req.query]);
+});
+const port = process.env.PORT || 3000
+app.listen(port, ()=> {
+    console.log(`app listenning on ${port}`)
 })
